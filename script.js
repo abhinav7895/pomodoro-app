d4533c9ad4bfaf8297667db3489128b471f60980
Abhinav Yadav <105491712+abhinav7895@users.noreply.github.com>
2023-11-18 11:46:43 +0530

    Update script.js
    Added comments, to make it more readable

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Buttons configuration
 const buttons = [
+    // Pomodoro button
     {
         element: document.querySelector("[data-pomodoro-btn]"),
         timeInSeconds: 1500,
-        color: "linear-gradient(#33d8d8, #0d0f92",
+        color: "linear-gradient(#33d8d8, #0d0f92)",
         background: "rgb(56, 110, 190)",
         msg: " - Time to focus!",
     },
+    // Short break button
     {
         element: document.querySelector("[data-shortBreak-btn]"),
         timeInSeconds: 300,
@@ -13,6 +16,7 @@ const buttons = [
         background: "rgb(255, 87, 87)",
         msg: " - Time for a short break!",
     },
+    // Long break button
     {
         element: document.querySelector("[data-longBreak-btn]"),
         timeInSeconds: 900,
@@ -22,6 +26,7 @@ const buttons = [
     },
 ];
 
+// Other elements and variables
 const startBtn = document.querySelector("[data-start-btn]");
 const pauseBtn = document.querySelector("[data-pause-btn]");
 const dataTimer = document.querySelector("[data-timer]");
@@ -30,8 +35,9 @@ const ringStart = document.querySelector('[data-ring="start"]');
 const body = document.querySelector("body");
 let msg;
 let timeInSeconds = buttons[0].timeInSeconds;
-const pomodoroWorker = new Worker('worker.js');
+const pomodoroWorker = new Worker('worker.js');  // Using a Web Worker for timer operations
 
+// Function to update the title
 function updateTitle(seconds, msg) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
@@ -39,6 +45,7 @@ function updateTitle(seconds, msg) {
     document.title = formattedTime + msg;
 }
 
+// Function to display time on the UI
 function displayTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
@@ -46,6 +53,7 @@ function displayTime(seconds) {
     dataTimer.textContent = formattedTime;
 }
 
+// Function to reset button colors
 function resetButtonColor() {
     buttons.forEach((btn) => {
         btn.element.style.background = "white";
@@ -53,12 +61,15 @@ function resetButtonColor() {
     });
 }
 
+// Function to start a Pomodoro session
 function startPomodoro(button) {
     msg = button.msg;
     resetButtonColor();
     pauseBtn.textContent = "PAUSE";
     timeInSeconds = button.timeInSeconds;
     displayTime(timeInSeconds);
+    pauseBtn.style.display = 'none';
+    // stop the previous worker timer
     pomodoroWorker.postMessage('stopTimer');
     startBtn.disabled = false;
     setTimeout(() => {
@@ -69,6 +80,7 @@ function startPomodoro(button) {
     updateTitle(timeInSeconds, msg);
 }
 
+// Event listeners for button clicks
 buttons.forEach((button) => {
     button.element.addEventListener("click", () => startPomodoro(button));
 });
@@ -86,12 +98,14 @@ pauseBtn.addEventListener("click", (e) => {
     }
 })
 
+// Function to start the timer
 function startTimer() {
     pomodoroWorker.postMessage('reset');
     startBtn.disabled = true;
     ringStart.play();
     pomodoroWorker.postMessage({ command: 'startTimer', timeInSeconds });
-    console.log(timeInSeconds);
+    pauseBtn.style.display = 'inline-block';
+    // Event listener for messages from the worker
     pomodoroWorker.addEventListener('message', (e) => {
         if (e.data.command === 'tick') {
             displayTime(e.data.timeInSeconds);
@@ -104,6 +118,8 @@ function startTimer() {
     });
 }
 
+// Initial setup
 startPomodoro(buttons[0])
 updateTitle(timeInSeconds, msg);
 displayTime(timeInSeconds);
+
